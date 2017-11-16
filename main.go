91700393a3caafec1aed2a5e304e6dc2003cf6e4
William Aristea Tantiono <waristea@gmail.com>
2017-11-17 00:13:14 +0700

    Add tester and fix infinite loop on POST request

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	//"time"
+	"fmt"
+	"time"
 	//"fmt"
 	"github.com/gorilla/context" // To use context
 	"gopkg.in/mgo.v2"            // To interact with mongoDB
@@ -11,14 +12,29 @@ import (
 	"net/http"
 )
 
+// for saving room and time for each subject
 type schedule struct {
 	//varname type			struct_tag
 	ID      bson.ObjectId `json:"id" bson:"_id"`
 	Subject string        `json:"subject" bson:"subject"`
 	Room    string        `json:"room" bson:"room"`
-	//TimeStart time.Time     `json:"timestart" bson:"timestart"`
-	//TimeEnd   time.Time     `json:"timeend" bson:"timeend"`
+	Daytime []daytime     `json:"daytime" bson:"daytime"`
 }
+
+//
+type daytime struct {
+	//varname type			struct_tag
+	Day       int       `json:"day" bson:"day"` // 1~7 : Mon~Sun
+	TimeStart time.Time `json:"timestart" bson:"timestart"`
+	TimeEnd   time.Time `json:"timeend" bson:"timeend"`
+}
+
+// to run MongoDB use :
+// mongod --dbpath="./db"
+
+// to use curl:
+// curl -i -X POST -d "{\"subject\":\"AKE\",\"room\":\"7601\"}"
+// http://localhost:8080/schedules
 
 // ADAPTER
 
@@ -71,6 +87,8 @@ func handle(w http.ResponseWriter, r *http.Request) {
 // Read
 func handleRead(w http.ResponseWriter, r *http.Request) {
 	// get db context (close is handled by adapted handler)
+	fmt.Println("Catch Read")
+
 	db := context.Get(r, "database").(*mgo.Session)
 	// load the schedules (sliced)
 	var schedules []*schedule
@@ -89,11 +107,14 @@ func handleRead(w http.ResponseWriter, r *http.Request) {
 // Insert
 func handleInsert(w http.ResponseWriter, r *http.Request) {
 	// get db context (close is handled by adapted handler)
+	fmt.Println("Catch Insert")
+
 	db := context.Get(r, "database").(*mgo.Session)
 	// decode the request body to 'schedule' struct
 	var s schedule
 
 	if err := json.NewDecoder(r.Body).Decode(&s); err != nil {
+		fmt.Println("Erorr1")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -101,16 +122,24 @@ func handleInsert(w http.ResponseWriter, r *http.Request) {
 	s.ID = bson.NewObjectId()
 	// insert it into the database
 	if err := db.DB("scheduleapp").C("schedules").Insert(&s); err != nil {
+		fmt.Println("Erorr2")
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	// redirect to it
-	http.Redirect(w, r, "/schedules/"+s.ID.Hex(), http.StatusTemporaryRedirect)
+	//http.Redirect(w, r, "http://localhost:8080/schedules/"+s.ID.Hex(), http.StatusTemporaryRedirect)
+	// loops around because the method is still post
+	//http.Redirect(w, r, "http://localhost:8080/schedules", http.StatusTemporaryRedirect)
+	//w.Header().Set("Content-Type", "application/json")
+
+	sout := []byte("Your input is accepted")
+	w.Write(sout)
 }
 
 // Main
 func main() {
 	// connect to the database
+	fmt.Println("Running")
 	db, err := mgo.Dial("localhost") //mgo.Dial returns an mgo.Session
 	if err != nil {
 		log.Fatal("cannot dial mongo ", err)
@@ -123,7 +152,7 @@ func main() {
 	// add the handler
 	http.Handle("/schedules", context.ClearHandler(h))
 	//ClearHandler will clean up any memory used by the context.Set method
-
+	// Hint : path.Base("/id/123") and split
 	// start the server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
---
+++ tester.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+	//"github.com/gorilla/context" // To use context
+	//"gopkg.in/mgo.v2"            // To interact with mongoDB
+	//"gopkg.in/mgo.v2/bson"
+	//"log"
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+// for saving room and time for each subject
+type Schedule struct {
+	//varname type			struct_tag
+	//ID      bson.ObjectId `json:"id" bson:"_id"`
+	Subject string    `json:"subject" bson:"subject"`
+	Room    string    `json:"room" bson:"room"`
+	Daytime []Daytime `json:"daytime" bson:"daytime"`
+}
+
+//
+type Daytime struct {
+	//varname type			struct_tag
+	Day       int       `json:"day" bson:"day"` // 1~7 : Mon~Sun
+	TimeStart time.Time `json:"timestart" bson:"timestart"`
+	TimeEnd   time.Time `json:"timeend" bson:"timeend"`
+}
+
+// Main
+func main() {
+
+	url := "http://localhost:8080/schedules"
+	/*
+		day1 := Daytime{
+			day:       1,
+			timestart: time.Now(),
+			timeend:   time.Now(),
+		}
+
+		day2 := Daytime{
+			day:       5,
+			timestart: time.Now(),
+			timeend:   time.Now(),
+		}
+	*/
+	schedule := Schedule{
+		Subject: "AKE",
+		Room:    "7601",
+		Daytime: []Daytime{
+			{
+				Day:       1,
+				TimeStart: time.Now(),
+				TimeEnd:   time.Now(),
+			},
+			{
+				Day:       5,
+				TimeStart: time.Now(),
+				TimeEnd:   time.Now(),
+			},
+		},
+	}
+
+	b, err := json.Marshal(schedule)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//fmt.Println(schedule)
+	//fmt.Println(b)
+	fmt.Println(string(b))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
+}
